1f1472f009e34653fbf0a3c14b936494232024fb
zeneke <zeneke@gmail.com>
2017-01-27 12:47:54 +0100

    added migrations, and Temp model to admin panel

--- arduinosite/calor/admin.py
+++ arduinosite/calor/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
-from . models import TemperaturaTest
+from . models import TemperaturaTest, Temperatura
 # Register your models here.
 admin.site.register(TemperaturaTest)
+admin.site.register(Temperatura)
 
